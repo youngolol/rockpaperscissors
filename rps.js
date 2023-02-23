801f3afd33a8eceec8b54d25177c36027131924f
Young Lee <lee.yghoon@gmail.com>
2023-02-23 01:20:31 -0500

    Finish js for playround with button input. Start CSS for styling

--- rps.js
+++ rps.js
@@ -1,49 +1,68 @@
 function numberGenerator(max) {
-    let num = Math.floor((Math.random() * max) + 1);
-    return num;
-};
+  let num = Math.floor(Math.random() * max + 1);
+  return num;
+}
 
 function getComputerChoice() {
-let randomNum = numberGenerator(3)
-if (randomNum === 3){
+  let randomNum = numberGenerator(3);
+  if (randomNum === 3) {
     return "rock";
-} else if (randomNum === 2){
+  } else if (randomNum === 2) {
     return "paper";
-} else if (randomNum === 1){
+  } else if (randomNum === 1) {
     return "scissors";
+  }
 }
-};
+
+let player_score = 0;
+let computer_score = 0;
+let tie_score = 0;
 
 const handleButtonClick = (event) => {
-  console.log(playRound(event.target.value, getComputerChoice()));
-}
-
-function playRound(playerSelection, computerSelection) {
-if (computerSelection === playerSelection){
-    return "tie";
-} else if (playerSelection === 'rock'){
-    if (computerSelection === 'paper') {
-      return "computer";
-    } else {
-      return "player";
-    }
-} else if (playerSelection === 'paper'){
-    if (computerSelection === 'rock') {
-      return "player";
-    } else {
-      return "computer";
-    }
-} else if (playerSelection === 'scissors'){
-    if (computerSelection === 'rock') {
-      return "computer";
-    } else {
-      return "player";
-    }
-}
+  let playRoundResult = playRound(event.target.value, getComputerChoice());
+  if (playRoundResult === "player") {
+    player_score++;
+    document.getElementById(
+      "player_score"
+    ).innerText = `Player = ${player_score}`;
+  } else if (playRoundResult === "computer") {
+    computer_score++;
+    document.getElementById(
+      "computer_score"
+    ).innerText = `Computer = ${computer_score}`;
+  } else if (playRoundResult === "tie") {
+    tie_score++;
+    document.getElementById("tie_score").innerText = `Tie = ${tie_score}`;
+  }
 };
 
-// console.log(playRound(playerSelection, computerSelection));
+// initialize counter
 
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection === playerSelection) {
+    return "tie";
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      return "computer";
+    } else {
+      return "player";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return "player";
+    } else {
+      return "computer";
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return "computer";
+    } else {
+      return "player";
+    }
+  }
+}
+
+// console.log(playRound(playerSelection, computerSelection));
 
 // function game(playRound){
 // let oneWinner = {};
